2edd0b2cdd984c65896e25941f0161e57016a9dd
SHYAM SHUNMUGAM G <160461398+ShyamG004@users.noreply.github.com>
2025-04-02 20:10:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ import path from 'path';
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello, World!' });
+});
 app.use('/uploads', express.static(path.join(path.resolve(), 'uploads')));
 app.use('/api/auth', authRoutes);
 app.use('/api/personal', personalRoutes);
